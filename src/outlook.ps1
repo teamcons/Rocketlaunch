4f351ee6bc4f51a9c9b4691261a3db27cb684f5e
teamcons_atwork <littlepastelworld@gmail.com>
2024-02-13 12:27:14 +0100

    searching thru outlook

--- src/outlook.ps1
+++ src/outlook.ps1
@@ -1,30 +1,59 @@
 
 
 $OL=New-Object -ComObject OUTLOOK.APPLICATION
-$ns =$OL.GETNAMESPACE("MAPI")
+$ns = $OL.GETNAMESPACE("MAPI")
 
 
 # Last email
-$mail = $ns.Folders.Item(1).Folders.Item("Posteingang").Items(1)
+#$mail = $ns.Folders.Item(1).Folders.Item("Posteingang").Items(1)
 # | Select-Object -Property Subject : Get who
 # .Attachments
 
 
 #$OL.AdvancedSearch("Posteingang","urn:schemas:httpmail:hasattachment=true ").Results[1]
-$mail = $ns.Folders.Item(1).Folders.Item("Posteingang").Items
+#$mail = $ns.Folders.Item(1).Folders.Item("Posteingang").Items
 
-$date = Get-Date (Get-Date).AddDays(-7) -Format 'd/MM/yyyy hh.mm tt'
-$filter = "[ReceivedTime] >= '$date'"
-$mail = $ns.Folders.Item(1).Folders.Item("Posteingang").Items.Restrict($filter)
+$date = Get-Date (Get-Date).AddDays(-2) -Format 'dd/MM/yyyy'
+$filter = "[ReceivedTime] >= '$date 18:00'"
+$allmails = $ns.Folders.Item(1).Folders.Item("Posteingang").Items.Restrict($filter)
 
 
 # Attachments of last mail
-$mail[1].Attachments
+#$mail[1].Attachments
 
 # Name 
-$mail[1].Attachments[1].FileName
+#$mail[1].Attachments[1].FileName
+
+# For one mail, get all true attachments
+#$mail[3] | Where-Object { $_.Attachments.FileName -notmatch "(image[0-9][0-9][0-9]|.msg)" }
+#$goodmails = ( $mail[3] | Where-Object { $_.Attachments.FileName -match "(.pdf|.doc|.xls)" } )
 
 
 
-.Items.Restrict('[UnRead] =   True')
-$mail | Select-Object -Property Body | Format-List
+
+foreach ($mail in $allmails)
+{
+
+    #[bool]$istrueattach = $false
+    foreach ( $attach in $mail.Attachments ) 
+    {
+        #echo $attach.FileName
+        if ($attach.FileName -match  "(.pdf|.doc|.xls)" )
+        {
+            echo "found !"
+            echo $mail.Subject
+            echo $mail.SenderName
+
+        }
+
+
+    }
+
+}
+
+
+
+
+#echo $goodmails.Subject
+
+
