61266d118cce4d7f5bc00302e7be06774130c1fe
teamcons_atwork <littlepastelworld@gmail.com>
2024-02-13 11:24:08 +0100

    outlook shenanigans

--- src/outlook.ps1
+++ src/outlook.ps1
@@ -1,15 +1,30 @@
-$olApp = new-object -comobject outlook.application
-$namespace = $olApp.GetNamespace("MAPI")
-$Folders = $namespace.Folders.Item("\\SKRIVANEK Belgium")
-
-#parent folder
-$Folder = $Folders.Folders.Item("Inbox")
-
-#$Folder.folders.Add($subfolder)
-  
-echo $Folder
 
 
+$OL=New-Object -ComObject OUTLOOK.APPLICATION
+$ns =$OL.GETNAMESPACE("MAPI")
 
-$olApp.Quit | Out-Null
-[GC]::Collect()
+
+# Last email
+$mail = $ns.Folders.Item(1).Folders.Item("Posteingang").Items(1)
+# | Select-Object -Property Subject : Get who
+# .Attachments
+
+
+#$OL.AdvancedSearch("Posteingang","urn:schemas:httpmail:hasattachment=true ").Results[1]
+$mail = $ns.Folders.Item(1).Folders.Item("Posteingang").Items
+
+$date = Get-Date (Get-Date).AddDays(-7) -Format 'd/MM/yyyy hh.mm tt'
+$filter = "[ReceivedTime] >= '$date'"
+$mail = $ns.Folders.Item(1).Folders.Item("Posteingang").Items.Restrict($filter)
+
+
+# Attachments of last mail
+$mail[1].Attachments
+
+# Name 
+$mail[1].Attachments[1].FileName
+
+
+
+.Items.Restrict('[UnRead] =   True')
+$mail | Select-Object -Property Body | Format-List
